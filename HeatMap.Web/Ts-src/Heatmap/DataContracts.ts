9a926aad971f7145068facf9953efdc43fee9272
amigin <33rkjgerhjg51>
2019-01-18 15:28:00 +0200

    adjusted minification bundle for two widgets

--- HeatMap.Web/Ts-src/Heatmap/DataContracts.ts
+++ HeatMap.Web/Ts-src/Heatmap/DataContracts.ts
@@ -1,15 +1,15 @@
 namespace Lykke.HeatMap {
 
-    
+
     export interface IOvershoot {
-        assetId:string;
-        thresholds : IOvershootThreshold[];
+        assetId: string;
+        thresholds: IOvershootThreshold[];
     }
-    
+
     export interface IOvershootThreshold {
         percent: number;
         delta: number;
         direction: string;
     }
-    
+
 }
